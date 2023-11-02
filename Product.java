0ed2fdc11df2a5426da31466bf7b1e4e9fb4e96a
AlessandroCiaccio <146941441+AlessandroCiaccio@users.noreply.github.com>
2023-11-02 15:11:53 +0100

    Added Product

---
+++ Product.java
@@ -0,0 +1,47 @@
+public class Product {
+    private String name;
+    private String manufacturer;
+    private long id;
+    private int quantity;
+
+    public Product(String name, String manufacturer, long id, int quantity) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.id = id;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public static void printManifacturerFirstLetters()
+}
